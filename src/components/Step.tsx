8e9108dc4dea57bf0ecdadc47243aaa7226517c4
buradabhargavi <bhargaviboorada246@gmail.com>
2024-05-21 10:07:56 +0530

    singleComponent

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
-import Count from "./Count";
 
 import "./Step.css";
 
 function Step() {
   const [step, setStep] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
+
+  const date = new Date();
+
+  const decHandler = () => {
+    setCount((count) => count - step);
+  };
+
+  const incHandler = () => {
+    setCount((count) => count + step);
+  };
+
+  const resetHandler = () => {
+    setCount(0);
+    setStep(1);
+  };
+
+  date.setDate(date.getDate() + count);
+  const dateString = date.toDateString();
 
   /*  const decHandler = () => {
     setStep((step) => step - 1);
@@ -14,18 +32,19 @@ function Step() {
   }; */
 
   return (
-    <div className="mainContainer">
-      <input
-        className="range"
-        type="range"
-        min="0"
-        max="10"
-        value={step}
-        onChange={(e) => setStep(Number(e.target.value))}
-      ></input>
-      <span>{step}</span>
+    <>
+      <div className="mainContainer">
+        <input
+          className="range"
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        ></input>
+        <span>{step}</span>
 
-      {/* <div className="step">
+        {/* <div className="step">
         <button onClick={decHandler} className="dec">
           -
         </button>
@@ -34,8 +53,38 @@ function Step() {
           +
         </button>
       </div> */}
-      <Count value={step}></Count>
-    </div>
+        <div className="step">
+          <button onClick={decHandler} className="dec">
+            -
+          </button>
+          <input
+            type="number"
+            value={count}
+            onChange={(e) => {
+              setCount(Number(e.target.value));
+            }}
+          ></input>
+          <button onClick={incHandler} className="inc">
+            +
+          </button>
+        </div>
+        <div>
+          <p>
+            {count === 0
+              ? `Today is`
+              : count > 0
+              ? `${count} days from today is`
+              : count < 0
+              ? `${-count} days ago the date was `
+              : ""}
+          </p>
+          <p className="datecls">{dateString}</p>
+        </div>
+        <div className="reset">
+          <button onClick={resetHandler}>reset</button>
+        </div>
+      </div>
+    </>
   );
 }
 
